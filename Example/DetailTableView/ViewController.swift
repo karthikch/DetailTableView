946379c4823e8f82d27f2a0828a44a2a2992ff8d
karthik ch <karthikkumar410@gmail.com>
2025-04-14 11:55:10 +0530

    initial cocoapod

---
+++ Example/DetailTableView/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  DetailTableView
+//
+//  Created by karthikch on 04/13/2025.
+//  Copyright (c) 2025 karthikch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
